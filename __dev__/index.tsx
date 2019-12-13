740df74e1ba2b6b84403882e6a2b044238d3bfcc
CadenGuo <411189077@qq.com>
2019-12-13 11:33:34 +0800

    clear developer customized test components

--- __dev__/index.tsx
+++ __dev__/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+/**
+ * You may use your own App.ts file here to test.
+ * Before commiting, please make sure all your customized testing components are in ./component folder
+ */
 import App from './App';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
