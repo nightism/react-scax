5661f5900634a7d87613cce827156a7d50d622da
Sun Mingyang <sun.mingyang@gmx.com>
2020-03-10 12:32:26 +0800

    export scaxer blocking options

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import attachFunc from './attach';
-import { AJAX_CALL_METHOD, SCAXER_STATE, TEMPLATE_TYPE } from './common/constants';
+import { AJAX_CALL_METHOD, SCAXER_BLOCKING, SCAXER_STATE, TEMPLATE_TYPE } from './common/constants';
 import { configureTemplate, createTemplatePromiseFunction } from './template/template';
 import { IAttach } from './types';
 
@@ -11,6 +11,7 @@ export const attach: IAttach = attachFunc;
 
 export const AjaxCallMethod = AJAX_CALL_METHOD;
 export const ScaxerState = SCAXER_STATE;
+export const ScaxerBlocking = SCAXER_BLOCKING;
 export const Template = {
    changeTemplateSettings: configureTemplate,
    createPromise: createTemplatePromiseFunction,
